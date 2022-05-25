3897bb52a4e3a78e381ab3963de5a80c2ae189e0
eduardo <ezafalon90@gmail.com>
2022-05-25 20:37:00 -0300

    up

---
+++ Facec.Progamacao.Async/Exemplo.CompartilharVariavelEstarica/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Exemplo.CompartilharVariavelEstarica
+{
+    internal class Program
+    {
+        private static bool _isImprimir = true;
+
+        static void Main(string[] args)
+        {
+            var threadUm = new Thread(ImprimirNome);
+            var threadDois = new Thread(ImprimirNome);    
+
+            threadUm.Start();
+            threadDois.Start();
+
+            Console.ReadLine();
+        }
+
+        private static void ImprimirNome()
+        {
+            if (!_isImprimir)
+            {
+                return;
+            }
+            _isImprimir = false;
+            Console.WriteLine("Eduardo");
+        }
+    }
+}
---
+++ Facec.Progamacao.Async/Exemplo.Threads/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace Exemplo.Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chamada sincrona!");
+
+            EscreverData();
+
+            Console.WriteLine("Chamada por thread!");
+
+            Thread threadUm = new Thread(EscreverData);
+            
+            threadUm.Start();
+
+            Console.WriteLine($"Código da Thread Primaria: {Thread.CurrentThread.ManagedThreadId}");
+
+            Console.WriteLine($"Código da Thread secundaria: {threadUm.ManagedThreadId}");
+
+            Thread threadDois=new Thread(EscreverNome);
+
+            threadDois.Start();
+
+            //Console.WriteLine($"Codigo da Thread Nome: {threadDois.ManagedThreadId}");
+
+            //Console.ReadLine(); 
+        }
+
+        //metodo sincrono
+        private static void EscreverData()
+        {
+
+            Console.WriteLine($"Data hora: {DateTime.Now}");
+        }
+        private static void EscreverNome() 
+        {
+            Console.WriteLine("Digite seu nome");
+             string nome = Console.ReadLine();
+
+            Console.WriteLine($"nome é {nome}");
+
+
+        }
+    }
+}
