0eb5c38824403da23733926d28908909e2ed6d82
eduardo <ezafalon90@gmail.com>
2022-05-25 22:25:27 -0300

    up

---
+++ Facec.Progamacao.Async/Exemplo.MetodoAssincrono/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exemplo.MetodoAssincrono
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EscreverNomeAsync();
+
+            EscreverNomeAguardavelAsync().GetAwaiter().GetResult();
+            
+            Console.WriteLine(ObterNomeAsync().GetAwaiter().GetResult());
+
+            Console.ReadLine();
+        }
+
+        private static async void EscreverNomeAsync()
+        {
+            await Task.Run(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));                          
+                Console.WriteLine("Eduardo");
+            });
+        }
+
+        private static async Task EscreverNomeAguardavelAsync()
+        {
+            await Task.Run(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(3));
+                Console.WriteLine("Eduardo");
+            });
+        }
+        private static async Task<string> ObterNomeAsync()
+        {
+            var result = string.Empty;
+
+            await Task.Run(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(4));
+                result = "Eduardo";
+            });
+
+            return result;
+        }
+    }
+}
---
+++ Facec.Progamacao.Async/Exemplo.Tasks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Exemplo.Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new Task<string>(() =>
+            {
+                try
+                {
+                    return "Eduardo";
+                }catch(Exception ex)
+                {
+                    return ex.Message;
+                }
+            });
+
+            var result = task.GetAwaiter().GetResult();
+            
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
